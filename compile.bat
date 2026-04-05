6dbfa65618cc35f96e93b3239591a3f7cb4e3d67
qpAndri59 <andreeamirela63@yahoo.com>
2026-04-06 00:17:31 +0300

    Fixes

--- compile.bat
+++ compile.bat
@@ -1,1 +1,3 @@
-g++ -g main.cpp resources.res -o .\out\Setup.exe -mwindows -lgdi32 -lcomctl32 -lshell32 -lole32 -luuid -static -static-libgcc -static-libstdc++
+windres resources.rc -o resources.o
+g++ -g main.cpp resources.o -o .\out\Setup.exe -mwindows -lgdi32 -lcomctl32 -lshell32 -lole32 -luuid -static -static-libgcc -static-libstdc++
+PAUSE
